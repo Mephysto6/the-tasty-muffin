e55c67d5fa7d6e03f9d57c8e9af2b60132814650
MarieHelene <mh.bury@gmail.com>
2025-02-04 22:08:17 +0100

    home page images

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,30 +1,50 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, ScrollView } from "react-native";
 import { Link } from "expo-router";
 import { styles } from "@/global" ;
-import ImageViewer from '@/components/ImageViewer';
-import Button from '@/components/Button';
+import ImageViewer from "@/components/ImageViewer";
+import Button from "@/components/Button";
+import React, { useEffect } from "react";
+// import { Link as scrollLink, animateScroll as scroll } from "react-scroll";
+import { DessertList } from "@/assets/images/desserts/index"
 
-const PlaceholderImage = require('@/assets/images/react-logo.png');
+const path = "@/assets/images/react-logo.png" ;
+const PlaceholderImage = require(path);
 
 export default function Index() {
+  useEffect(() => {
+    document.title = "The Tasty Muffin";
+  }, []);
+  const dessert_list:string[] = ["cookies", "muffins"] ;
+  const dessert_images = [] ;
+  
+  for (let i=0 ; i < DessertList.length ; i++) {
+    dessert_images.push(
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={DessertList[i]} />
+      </View>
+    )
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>
+    <ScrollView style={styles.container}>
+
+      <Text style={styles.title}>
         The Tasty Muffin
       </Text>
-      <Link href="/search" style={styles.buttonS}>
-        Go to search for a dessert !
-      </Link>
       <Text style={styles.text}>
-        Or just browse :3
+        This website serves as a portfolio of my desserts. Feel free to browse.<br/>
+        /!\ very much still under construction /!\
+        {/* , and when you feel like it, add some to your cart and order ! */}
       </Text>
-      <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceholderImage} />
-      </View>
-      <View style={styles.footerContainer}>
+
+      {dessert_images}
+      
+    
+
+      {/* <View style={styles.footerContainer}>
         <Button theme="primary" label="Choose a photo" />
         <Button label="Use this photo" />
-      </View>
-    </View>
+      </View> */}
+    </ScrollView>
   );
 }
---
+++ assets/images/desserts/index.tsx
@@ -0,0 +1,3 @@
+export const Cookie0 = require("./cookies/0.png")
+export const Muffin0 = require("./muffins/0.png")
+export const DessertList = [Cookie0, Muffin0]
